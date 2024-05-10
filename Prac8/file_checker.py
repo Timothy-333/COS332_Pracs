13e567b434c59d0019bee6abdce0f4a4f18e5e0e
Tim Tam <timothymdevilliers@gmail.com>
2024-05-10 23:39:20 +0200

    Made code bit more reader friendly

--- Prac8/file_checker.py
+++ Prac8/file_checker.py
@@ -28,8 +28,15 @@ def send_command(sock, cmd):
     sock.sendall((cmd + "\r\n").encode())
     return sock.recv(1024).decode()
 
-def set_pasv(sock):
-    global ip_address, port_number, data_sock
+sock = None
+def set_pasv():
+    global sock
+    if sock:
+        sock.close()
+    sock = socket.create_connection((FTP_HOST, FTP_PORT))
+    sock.recv(1024).decode()
+    send_command(sock, "USER " + FTP_USER)
+    send_command(sock, "PASS " + FTP_PASS)
     response = send_command(sock, "PASV")
     start = response.find("(") + 1
     end = response.find(")")
@@ -39,9 +46,9 @@ def set_pasv(sock):
     return socket.create_connection((ip_address, port_number))
 
 # Function to download a file from the FTP server
-def download_file(sock, remote_file_path, local_file_path):
+def download_file(remote_file_path, local_file_path):
     print("Downloading file from FTP server...")
-    data_sock = set_pasv(sock)
+    data_sock = set_pasv()
     # Send the RETR command over the control connection
     print(send_command(sock, "RETR " + remote_file_path))
     # Receive the file data over the data connection
@@ -56,17 +63,13 @@ def download_file(sock, remote_file_path, local_file_path):
 
 try:
     while True:
-        sock = socket.create_connection((FTP_HOST, FTP_PORT))
-        sock.recv(1024).decode()
-        send_command(sock, "USER " + FTP_USER)
-        send_command(sock, "PASS " + FTP_PASS)
         # Check if the local file exists
         if not os.path.exists(LOCAL_FILE_PATH):
             print("File was Deleted")
-            download_file(sock, REMOTE_FILE_PATH, LOCAL_FILE_PATH)
+            download_file(REMOTE_FILE_PATH, LOCAL_FILE_PATH)
         else:
             # If it does, calculate its hash
-            data_sock = set_pasv(sock)
+            data_sock = set_pasv()
             local_file_hash = calculate_hash(LOCAL_FILE_PATH)
             send_command(sock, "RETR " + REMOTE_HASH_PATH)
             # Receive the file data over the data connection
@@ -85,15 +88,11 @@ try:
             data_sock.close()
             if local_file_hash != remote_file_hash:
                 print("File was Edited")
-                sock.close()
-                sock = socket.create_connection((FTP_HOST, FTP_PORT))
-                sock.recv(1024).decode()
-                send_command(sock, "USER " + FTP_USER)
-                send_command(sock, "PASS " + FTP_PASS)
-                download_file(sock, REMOTE_FILE_PATH, LOCAL_FILE_PATH)
+                download_file(REMOTE_FILE_PATH, LOCAL_FILE_PATH)
         sock.close()
         time.sleep(10)
 finally:
     if data_sock:
         data_sock.close()  # Close the data connection
-    sock.close()  # Close the control connection
+    if sock:
+        sock.close()  # Close the control connection
