fbc45646d690681ec5c98114388e63f43dcd9012
Tim Tam <timothymdevilliers@gmail.com>
2024-05-30 00:13:26 +0200

    updated the smtp

--- Prac9/smtp_proxy.py
+++ Prac9/smtp_proxy.py
@@ -17,16 +17,16 @@ class SMTPProxyServer:
 
     def handle_client(self, client_socket):
         logging.info("Client connected")
-    
+
         try:
             # Connect to the real SMTP server and get its greeting
             with socket.create_connection((REAL_SMTP_HOST, REAL_SMTP_PORT)) as smtp_socket:
                 server_greeting = smtp_socket.recv(1024)
                 logging.debug("Server greeting: %s", server_greeting.decode().strip())
-    
+
                 # Send server greeting to client
                 client_socket.sendall(server_greeting)
-    
+
                 # Receive and forward subsequent commands and email data
                 while True:
                     data = client_socket.recv(1024)
@@ -35,13 +35,24 @@ class SMTPProxyServer:
                     smtp_socket.sendall(data)
                     reply = smtp_socket.recv(1024)
                     client_socket.sendall(reply)
-    
+
+                    # If the client sent the DATA command and the server responded with 354,
+                    # wait for the client to send the email data and then send the termination sequence
+                    if b"DATA" in data and b"354" in reply:
+                        while True:
+                            data = client_socket.recv(1024)
+                            if not data or data.endswith(b'\r\n.\r\n'):
+                                break
+                            smtp_socket.sendall(data)
+                        smtp_socket.sendall(b'\r\n.\r\n')
+                        reply = smtp_socket.recv(1024)
+                        client_socket.sendall(reply)
         except Exception as e:
-            logging.error("Error handling client: %s", e)
-    
+            logging.exception("Exception handling client")
         finally:
+            print("Message Sent")
             client_socket.close()
-            logging.info("Client disconnected")
+    
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
